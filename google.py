29f9f89d781d861d8492d803a12eaf3ab5c10ff9
officialjoemayami <46620364+officialjoemayami@users.noreply.github.com>
2019-11-18 01:58:20 +0100

    What's New?
    Aida can now effectively search through vairous url, and provide error message if data is not found, when data is not found, this mean the webscraping structure for the url has not be developed yet. In future i hope i can intergrate and AI ability in Aida that can make her auto update her code herself. But in meantime, i have to do that mannually.
    Aida can also sort the data acquired from the webpage and next feature to expect is to get the top 5 companies from the data Aida has.

--- google.py
+++ google.py
@@ -3,53 +3,114 @@ try:
     from googlesearch import search
     from bs4 import BeautifulSoup
     import requests
+    import csv
 except ImportError:
     print("A library is not found")
 
-# search google
-keyword = "Logo Design"
-query = "Top 5 " + keyword + "companies"
-res = []
+comData = []
 
-# run google search
-for j in search(query, tld="com", num=10, stop=10, pause=2):
-    res.append(j)
+# GET Product or Niche Keyword from user and validate it.
+def getKeyword():
+    keyword = input("What is your product/niche? ")
+    if keyword.strip() != "":
+        if type(keyword) == type(str()):
+           return keyword
+    return None
 
+# Search for the keyword based on STAGE 1 query
+def searchGoogle():
+    res = []
+    keyword = getKeyword()
+    print(keyword)
+    if keyword != None:
+        query = "Top 5 " + keyword + "companies"
+        # run google search
+        for j in search(query, tld="com", num=10, stop=10, pause=2):
+            res.append(j)
+    return res
 
-url = res[1]
-
-# send a request
-r = requests.get(url)
-
-# structuring data for scrapping
-soup = BeautifulSoup(r.content, 'html5lib')
-
-
-companyData = []  # a list of company data 
-
-# getting data
-data = soup.find('div', attrs = {'class':'view-content container'}) 
-
-
-# finding company info
-for row in data.findAll('li', attrs = {'class':'views-row'}):
-    companyDat = {}
-    companyDat['name'] = row.h3.a.span.text.strip()
-    companyDat['link'] = row.h3.a['href'].strip()
-    companyDat['rating'] = row.find('div', attrs = {'class':'widget-total-reviews'})
-    companyDat['reviews'] = row.find('div', attrs = {'class':'widget-reviews-count'})
-    companyData.append(companyDat)
-
-for data in companyData:
-    if(data['rating'] != None):
-        data['rating'] = data['rating'].text.strip()
+# GET data from search results
+def getData():
+    res = searchGoogle()
+    print(res)
+    if len(res) != 0:
+        for url in res:
+            r = requests.get(url)
+            # structuring data for scrapping
+            soup = BeautifulSoup(r.content, 'html5lib')
+            data = findData(soup)
+            # print(data)
+            if data != None:
+                rows = findRows(data)
+                for row in rows:
+                    companyData = {}
+                    companyData['name'] = getCompanyName(row)
+                    companyData['link'] = getCompanLink(row)
+                    companyData['rating'] = getCompanyRating(row)
+                    companyData['reviews'] = getCompanyReviews(row)
+                    comData.append(companyData)
+                    cleanData()
+            else:
+                print("Data not Found")
     else:
-        companyData.remove(data)
+        print("No search result on this product/niche")
+    return comData
 
-for data in companyData:
-    if(data['reviews'] != None):
-        data['reviews'] = data['reviews'].find('span', attrs = {'class':'count'}).text.strip()
-    else:
-        companyData.remove(data)
+# Find Data from Webpage
+def findData(soup):
+    if soup.find('div', attrs = {'class':'view-content container'}) != None:
+        return soup.find('div', attrs = {'class':'view-content container'})
+    return None
 
-print(companyData)
+# Find row containing needed data
+def findRows(data):
+    if data.findAll('li', attrs = {'class':'views-row'}) != None:
+        return data.findAll('li', attrs = {'class':'views-row'})
+    return None
+
+# GET company name from row
+def getCompanyName(row):
+    if row.h3.a.span.text.strip() != None:
+        return row.h3.a.span.text.strip()
+    return None
+# GET company website url link from row
+def getCompanLink(row):
+    if row.h3.a['href'].strip() != None:
+        return row.h3.a['href'].strip()
+    return None
+# GET company rating from row
+def getCompanyRating(row):
+    if row.find('div', attrs = {'class':'widget-total-reviews'}) != None:
+        rating = row.find('div', attrs = {'class':'widget-total-reviews'})
+        return rating.text.strip()
+    return None
+# GET company reviews from row
+def getCompanyReviews(row):
+    if row.find('div', attrs = {'class':'widget-reviews-count'}) != None:
+        reviews = row.find('div', attrs = {'class':'widget-reviews-count'})
+        return reviews.find('span', attrs = {'class':'count'}).text.strip()
+    return None
+
+# Cleaning data by removing null fields and nosie
+def cleanData():
+    for data in comData:
+        if(data['rating'] == None):
+            comData.remove(data)
+
+# Save data as csv
+def exportCSV():
+    organize = sorted(comData, key = lambda i: i['rating'],reverse=True) # Organizing data by sorting it desc
+    # print(organize)
+    filename = 'companyData.csv'
+    with open(filename, 'w') as f:
+        w = csv.DictWriter(f,['name','link','rating','reviews']) 
+        w.writeheader()
+        if len(organize) != 0:
+            for data in organize:
+                w.writerow(data)
+        else: print('Data is empty')
+    print("Export to csv completed")
+
+if __name__ == '__main__':
+    comData = getData()
+    exportCSV()
